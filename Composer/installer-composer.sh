29f15ac7f5ad52a6e109525772a643e984a0e167
maiconparra <maiconparradasilva@gmail.com>
2021-08-17 09:01:07 -0300

    Add script installer-composer

---
+++ Composer/installer-composer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt install -y php-cli uzip
+cd ~
+curl -sS https://getcomposer.org/installer -o composer-setup.php
+HASH=`curl -sS https://composer.github.io/installer.sig`
+echo $HASH
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+
